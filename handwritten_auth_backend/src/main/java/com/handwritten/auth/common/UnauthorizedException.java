06f754a8325bb8d5a34a14de9064e548899dcab6
xitihang <133010357+xitihang@users.noreply.github.com>
2025-11-11 20:25:29 +0800

    Delete handwritten_auth_backend directory

--- handwritten_auth_backend/src/main/java/com/handwritten/auth/common/UnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-package com.handwritten.auth.common;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
-
-
--- handwritten_auth_backend/src/main/java/com/handwritten/auth/user/repo/AuthUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.handwritten.auth.user.repo;
-
-import com.handwritten.auth.user.entity.AuthUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthUserRepository extends JpaRepository<AuthUser, Long> {
-}
-
-
